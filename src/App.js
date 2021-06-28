ef0927a40bdcbfd38202d3fa9ef97ea9a7ddcfdf
jamalashrafi <jamalnabi07@gmail.com>
2021-06-28 17:28:50 +0530

    add: added Display Card and Header to the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Header from './components/Header';
+import DisplayCard from './components/DisplayCard';
+import appCss from './App.module.css'
+import OrderCard from './containers/OrderCard';
 
 function App() {
   return (
-    <div className="App">
-      Food App
-    </div>
+    <>
+      <Header />
+      <DisplayCard styleCard={appCss['styleCard']}>
+        <h1>Delicious Food, Delivered To You</h1>
+        <p>Choose your favorite food from our broadn selection of available meals and enjoy a delicious
+        or Dinner at home.</p>
+        <p>All meals are cooked with high quality ingredients, just-in-time and of-course by experinced chefs.</p>
+      </DisplayCard>
+      <DisplayCard styleCard={appCss['menu-card']}>
+        <OrderCard />
+      </DisplayCard>
+    </>
   );
 }
 
