a528bfdc44e9819e914d0f590752821c7d299e18
jamalashrafi <jamalnabi07@gmail.com>
2021-06-28 17:33:01 +0530

    add: added container and component for the MenuCard.

---
+++ src/components/DisplayMenuItems.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import menuItem from './DisplayMenuItems.module.css';
+
+const DisplayMenuItems = (props) => {
+    const increaseQty = (e) => {
+        debugger
+        props.increaseItemQty(props.menu.Name);
+    }
+    return (
+        <div className={menuItem['style-menu-items']}>
+            <div>
+                <p className={menuItem['style-menu-name']}>{props.menu.Name}</p>
+                <p className={menuItem['style-menu-description']}>{props.menu.Description}</p>
+                <p className={menuItem['style-menu-price']}>$ {props.menu.Price}</p>
+            </div>
+            <div>
+                <div>
+                    Amount    <span className={menuItem['style-menu-amount']}>{props.menu.qty}</span>
+                </div>
+                <button onClick={increaseQty}>+ Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default DisplayMenuItems
